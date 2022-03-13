74cc500033948ad19048cd7b1ba74c70693cf9ed
leprime <bawagab@gmail.com>
2022-03-13 10:19:04 -0700

    exercise ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 /* Printing text */
 #include <stdio.h>
 
-int main(void) /* Main function */
+int main(void) 
+	/* Main function
+	 * puts is a printing fonction
+	 */
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
