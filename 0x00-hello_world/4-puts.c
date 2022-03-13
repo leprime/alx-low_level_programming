a46ca94cc08e1a55d670fa6223c0b50b109e964e
leprime <bawagab@gmail.com>
2022-03-13 10:43:13 -0700

    exercise ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main function for printing 
- * Return 
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
